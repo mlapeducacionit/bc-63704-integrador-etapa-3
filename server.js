b6d49691e11d7f1b42282cc71458855faa83f3cd
Maximiliano Principe <mlapeducacionit@gmail.com>
2023-10-20 13:03:48 -0300

    Agregamos las rutas y controladores de productos

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import 'dotenv/config'
+import routerProductos from './routers/productos.router.js'
+
+// ! CONFIGURACIONES
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// ! CONEXIÓN MONGODB
+
+// ! MIDDLEWARES
+app.use(express.urlencoded({extended: true})) // Decodifica el body enviado desde un form
+app.use(express.json()) // Decodificar el body enviado desde un json
+
+// ! ROUTES
+app.use('/api/productos', routerProductos)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
