42289ea43a09700258febd4b3fe2a7867b85f9c6
Maximiliano Principe <mlapeducacionit@gmail.com>
2023-11-08 11:27:16 -0300

    Modificamos el upload controller para que devuelva la ruta completa de la imagen

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -5,8 +5,13 @@ const uploadImagen = (req, res) => {
     if( !file ) {
         return res.status(400).send('No se recibió ninguna imagen')
     }
-    
-    res.status(201).json({foto: file.filename})    
+
+    const urlCompletaBack = `${req.protocol}://${req.get('host')}/uploads/${file.filename}`
+    console.log(req.protocol) // http o https
+    console.log(req.get('host')) // localhost:8080
+    console.log(file.filename) // nombre del archivo
+    console.log(urlCompletaBack)
+    res.status(201).json({foto: urlCompletaBack})    
 }
 
 export default {
